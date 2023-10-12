52b5c6e23fd6009bbe5680af244f6d0bc0d1dc7d
Wissal Loutfi <147111691+loutfiwissal@users.noreply.github.com>
2023-10-12 02:26:52 +0100

    exercise-sign

---
+++ exercise-sign.py
@@ -0,0 +1,7 @@
+A=int(input("taper le premier nombre: "))
+B=int(input("taper le deuxieme nombre: "))
+Z=A*B
+if Z>0 :
+   print("A et B ont le même signe")
+else :
+    print("A et B n'ont pas le même signe")
